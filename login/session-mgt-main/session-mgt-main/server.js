cc35af8f9c5ecb8de30fd0a8f01e23cdd6939ed5
Naveen <naveenuusa@gmail.com>
2023-05-12 21:58:14 +0530

    finalcode with mongodb connection

---
+++ login/session-mgt-main/session-mgt-main/server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const env = require('dotenv').config()
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+
+const passportConfig = require('./config/passportConfig');
+const cookieSession = require('cookie-session');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('passport');
+const keys = require('./config/keys')
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+
+
+mongoose.connect('mongodb+srv://naveenuusa20:naveenuusa1@cluster0.mkepvhf.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err) => {
+  if (!err) {
+    console.log('MongoDB Connection Succeeded.');
+  } else {
+    console.log('Error in DB connection : ' + err);
+  }
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');	
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(express.static(__dirname + '/views'));
+
+var index = require('./routes/index');
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, function () {
+  console.log('Server is started on http://127.0.0.1:'+PORT);
+});
+
+
+
+
